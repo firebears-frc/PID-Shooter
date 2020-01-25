d57d32f6057be1b25dd2f9b973b25ac2cafff880
DougLau <doug.p.lau@gmail.com>
2020-01-25 10:41:52 -0600

    Use SRX  built-in PID

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,29 +1,21 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.PIDCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import frc.robot.subsystems.ShooterSubsystem;
 
 public class RobotContainer {
 
-    private final PIDController controller;
-
     private final ShooterSubsystem shooterSubsystem;
 
     public RobotContainer() {
-        controller = new PIDController(1.0, 0.0, 0.0);
-        shooterSubsystem = new ShooterSubsystem(controller);
-        shooterSubsystem.setSetpoint(0.5);
+        shooterSubsystem = new ShooterSubsystem();
         shooterSubsystem.setDefaultCommand(getAutonomousCommand());
     }
 
     public Command getAutonomousCommand() {
-        return new PIDCommand(
-            controller,
-            () -> 0.2,
-            0.1,
-            (x) -> shooterSubsystem.output(x),
+        return new RunCommand(
+            () -> shooterSubsystem.periodic(),
             shooterSubsystem
         );
     }
--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,29 +1,47 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import edu.wpi.first.wpilibj.controller.PIDController;
-import edu.wpi.first.wpilibj2.command.PIDSubsystem;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ShooterSubsystem extends PIDSubsystem {
+public class ShooterSubsystem extends SubsystemBase {
 
-    private final WPI_TalonSRX srx;
+    static private final int PID_LOOP_IDX = 0;
+    static private final int TIMEOUT_MS = 30;
+    static private final double P = 1.0;
+    static private final double I = 0.0;
+    static private final double D = 0.0;
+    static private final double F = 0.0;
+    static private final double RPM = 200.0;
 
-    public ShooterSubsystem(PIDController c) {
-        super(c);
-        srx = new WPI_TalonSRX(12);
+    // private final WPI_TalonSRX srx;
+    private final TalonSRX srx;
+
+    public ShooterSubsystem() {
+        super();
+        srx = new TalonSRX(12);
+        srx.configFactoryDefault();
+        srx.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder,
+                                         PID_LOOP_IDX,
+                                         TIMEOUT_MS);
+        srx.setSensorPhase(true);
+        // Configure nominal / peak outputs
+        srx.configNominalOutputForward(0, TIMEOUT_MS);
+        srx.configNominalOutputReverse(0, TIMEOUT_MS);
+        srx.configPeakOutputForward(1, TIMEOUT_MS);
+        srx.configPeakOutputReverse(1, TIMEOUT_MS);
+        // Config PIDF
+        srx.config_kP(PID_LOOP_IDX, P, TIMEOUT_MS);
+        srx.config_kI(PID_LOOP_IDX, I, TIMEOUT_MS);
+        srx.config_kD(PID_LOOP_IDX, D, TIMEOUT_MS);
+        srx.config_kF(PID_LOOP_IDX, F, TIMEOUT_MS);
     }
 
-    @Override
-    protected double getMeasurement() {
-        return 0.2;
-    }
-
-    @Override
-    protected void useOutput(double output, double setpoint) {
-        srx.pidWrite(output);
-    }
-
-    public void output(double output) {
-        srx.pidWrite(output);
+    public void periodic() {
+        double output = srx.getMotorOutputPercent();
+        // velocity in units per 100 ms
+        double targetVelocity = RPM * 4096 / 600;
+        srx.set(ControlMode.Velocity, targetVelocity);
     }
 }
