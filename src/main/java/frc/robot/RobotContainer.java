8e3edb7baac1bbe1fc33ec4f0e4cc45047505c78
Theo-X <57161038+Theo-X@users.noreply.github.com>
2020-02-11 19:17:18 -0600

    ?

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,15 +2,18 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
+import frc.robot.subsystems.LidarLite;
 import frc.robot.subsystems.ShooterSubsystem;
 
 public class RobotContainer {
 
     private final ShooterSubsystem shooterSubsystem;
+    private final LidarLite lidarLite;
 
     public RobotContainer() {
         shooterSubsystem = new ShooterSubsystem();
         shooterSubsystem.setDefaultCommand(getAutonomousCommand());
+        lidarLite = new LidarLite();
     }
 
     public Command getAutonomousCommand() {
